3c97c3cf0831de8b39229695d1fa7ed4ef1da3c7
Bereket <blibanos@redhat.com>
2021-11-22 14:46:33 -0600

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+From node:16-alpine as builder
+workdir '/app'
+copy package.json .
+run npm install
+copy . .
+run npm run build
+
+From nginx 
+copy --from=builder /app/build /usr/share/nginx/html
+
--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Men You are doing greate brother. Really Though
         </p>
         <a
           className="App-link"
