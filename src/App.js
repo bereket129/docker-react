ed1b271d43c63b46bfb7d56c3f86e43c00a8db11
Bereket <blibanos@redhat.com>
2021-11-22 18:05:26 -0600

    changed app.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          I was changed Man
+          Men nobody can stop you, you are doing great bro. 
         </p>
         <a
           className="App-link"
